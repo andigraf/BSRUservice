cc978ac91cf0b6d69b575d53990a31c017752b21
andigraf <narawut4@hotmail.com>
2019-02-26 12:15:57 +0700

    Initial commit

---
+++ app/src/main/java/narawut/rukhomkaew/bsru/ac/th/MainActivity.java
@@ -0,0 +1,19 @@
+package narawut.rukhomkaew.bsru.ac.th;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     // Add Fragment to Activity
+        getSupportFragmentManager().beginTransaction().add(R.id.layoutMainFragment, new MainFragment()).commit();
+
+
+
+    } // Main Method
+} // Main Class
